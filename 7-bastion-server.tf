659602a2e34e9c20462aafb86649d24e1cadd7f8
Rahul Varma <rv111421@gmail.com>
2026-09-06 22:02:17 +0530

    corrected format

--- 7-bastion-server.tf
+++ 7-bastion-server.tf
@@ -94,9 +94,9 @@ resource "aws_security_group" "bastion" {
 
   egress {
     description = "Allow HTTPS and other outbound traffic"
-    from_port = 0
-    to_port   = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [
       "0.0.0.0/0"
     ]
